4cc27233475da9fcf412c9ce856899007e2a71cf
Sangmin Kim <plantstoen@gmail.com>
2019-08-26 12:23:41 +0900

    modify dimigoin_quick button, input box size

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -8,14 +8,24 @@
     <input type="text" placeholder="디미고인 아이디" class="loginbox__text"><br>
     <input type="password" placeholder="디미고인 비밀번호" class="loginbox__text">
   </div>
-  <span class="loginform__textbutton" @click="$router.push({name: 'main_page'})">디미고 계정으로 로그인</span>
-  <button><img src="./../../assets/dimigoin_logo.png">디미고인 바로가기</button>
+  <span class="loginform__textbutton" @click="$router.push({name: 'main_page'})">
+    디미고 계정으로 로그인
+  </span><br>
+  <button class="loginform__dminbutton" @click="openNewtap()">
+    <img src="./../../assets/dimigoin_logo.png" id="dminlogo">
+    디미고인 바로가기
+  </button>
 </div>
 </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    openNewtap() {
+      window.open('https://dimigo.in')
+    },
+  },
 
 }
 </script>
@@ -45,11 +55,28 @@ export default {
       cursor: pointer;
     }
   }
+  &__dminbutton{
+    margin-top: 6em;
+    width: 100%;
+    text-align: left;
+    font-size: 1em;
+    font-family: 'NanumSquare',sans-serif;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    background-color: white;
+    border-radius: 0.4em;
+    border: 0.06em solid rgb(211, 211, 211);
+    outline-style: none;
+    &:hover{
+      cursor: pointer;
+    }
+  }
 }
 .loginbox{
   margin-top: 3em;
   &__text{
-    width: 100%;
+    width: 96%;
     height: 2.8em;
     margin-top: 0.4em;
     margin-bottom: 0.4em;
@@ -64,5 +91,8 @@ export default {
     font-family: 'NanumSquare',sans-serif;
     font-weight: 400;
   }
+}
+#dminlogo{
+  width: 1.8em;
 }
 </style>
